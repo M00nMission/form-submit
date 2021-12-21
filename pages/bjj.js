f7a488f5a528efcd5da1ce1eb57809c4ba4ec45c
GoGetterMeme <4UXX2wMa9BuNJ5KBiKW8@tutanota.com>
2021-12-21 15:23:16 -0500

    Nuke everything

--- pages/bjj.js
+++ pages/bjj.js
@@ -1,87 +1,45 @@
 /* eslint-disable */
-import React, {useState, useCallback} from 'react';
-import {useDropzone} from 'react-dropzone'
-
+if ( typeof window !== 'undefined') {
+    function encode(data) {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&")
+      }
+    
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          "form-name": event.target.getAttribute("name"),
+          ...name
+        })
+      }).then(() => navigate("/thank-you/")).catch(error => alert(error))
+    }
+}
 
 export default function BJJ() {
-  const [name,setName] = useState('');
-  const [status,setStatus] = useState('');
-  const [email,setEmail] = useState('');
-  const [message,setMessage] = useState('');
-  const [file, setFile] = useState({});
+    return(
+        <>
+            <form name="BJJ" data-netlify="true">
+            <input type="hidden" name="BJJ" value="BJJ" />
+            <p>
+                <label>
+                <span>Name:</span>
+                <input name="name" type="text"/>
+                </label>
+            </p>
+            <p>
+                <label>
+                <span>Add file:</span>
+                <input name="file" type="file" accept="mp4"/>
+                </label>
+            </p>
+            <button type="submit">Submit</button>
+            </form>
+            <p className="result"></p>
+        </>
 
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-    setFile(acceptedFiles[0])
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k)=>{
-      formData.append(k,data[k])
-    });
-    return formData
-  }
-
-  const handleSubmit = e => {
-    const data = { "form-name": "contact", name, email, message, file }
-    
-    fetch("/", {
-      method: "POST",
-      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
-      body: encode(data)
-    })
-      .then(() => setStatus("Form Submission Successful!!"))
-      .catch(error => setStatus("Form Submission Failed!"));
-
-    e.preventDefault();
-  };
-
-  const handleChange = e => {
-    const {name, value} = e.target
-    if (name === 'name' ){
-      return setName(value)
-    }
-    if (name === 'email' ){
-      return setEmail(value)
-    }
-    if (name === 'message' ){
-      return setMessage(value)
-    }
-  }
-
-  return (
-    <div className="BJJ">
-    <form onSubmit={handleSubmit} action="/thank-you/">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={handleChange} />
-            </label>
-          </p>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-              isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-          </div>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-        <h3>{status}</h3>
-    </div>
-  );
+    )
 }
